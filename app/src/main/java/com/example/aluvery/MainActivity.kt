e9738bf88234b2ed1a43c43c1d60b31e9f333c8e
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-08-29 14:36:15 -0300

    Primeiros testes com row, column e box.

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,16 +27,68 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun ColumnPreview(){
+    Column {
+        Text("Texto 1")
+        Text("Texto 2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RowPreview(){
+    Row {
+        Text("Texto 1")
+        Text("Texto 2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoxPreview(){
+    Box {
+        Text("Texto 1")
+        Text("Texto 2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomLayoutPreview(){
+    Column {
+        Text("Texto 1")
+        Text("Texto 2")
+        Row {
+            Text("Texto 3")
+            Text("Texto 4")
+        }
+        Box {
+            Row {
+                Text("Texto 5")
+                Text("Texto 6")
+            }
+            Column {
+                Text("Texto 7")
+                Text("Texto 8")
+            }
+        }
+    }
+}
+
 @Composable
 fun MyFirstComposable(){
     Text(text = "Meu primeiro texto")
     Text(text = "Meu segundo texto maior")
 }
 
-@Preview(name = "TextPreviewLight",
+@Preview(
+    name = "TextPreviewLight",
     showSystemUi = true
 )
-@Preview(name = "TextPreviewDark",
+@Preview(
+    name = "TextPreviewDark",
     showSystemUi = true,
     uiMode = UI_MODE_NIGHT_YES
 )
