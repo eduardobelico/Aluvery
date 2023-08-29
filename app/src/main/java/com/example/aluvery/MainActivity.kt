814da148185f59d7a47b73a9a3992fea07cfc1f9
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-08-29 17:10:25 -0300

    Adequando codigo ao projeto do figma. Uso de modifiers para ajustar medidas, cor e alinhamento.

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -1,26 +1,31 @@
 package com.example.aluvery
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.aluvery.ui.theme.AluveryTheme
+import com.example.aluvery.ui.theme.Purple500
+import com.example.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    CustomLayoutPreview()
+                    ProductItem()
                 }
             }
             
@@ -36,144 +41,37 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun MyFirstComposable() {
-    Text(text = "Meu primeiro texto")
-    Text(text = "Meu segundo texto maior")
-}
-
-//Modifiers - primeiro parâmetro opcional
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun CustomLayoutPreview() {
+fun ProductItem() {
     Column(
         Modifier
-            .padding(8.dp)
-            .background(color = Color.Cyan)
-            .padding(all = 8.dp)
-            .fillMaxWidth()
-            .fillMaxHeight()
+            .height(250.dp)
+            .width(200.dp)
     ) {
-        Text("Texto 1")
-        Text("Texto 2")
-        Row(
-            modifier = Modifier
-                .padding(
-                    horizontal = 8.dp,
-                    vertical = 16.dp
-                )
-                .background(Color.LightGray)
-                .height(100.dp)
-                .fillMaxWidth(0.5f)
-        ) {
-            Text("Texto 3")
-            Text("Texto 4")
-        }
         Box(
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxWidth()
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Purple500, Teal200
+                        )
+                    )
+                )
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Imagem do produto",
             Modifier
-                .padding(8.dp)
-                .background(color = Color.Magenta)
-                .padding(all = 8.dp)
-                .width(200.dp)
-        ) {
-            Row(
-                Modifier
-                    .padding(8.dp)
-                    .background(color = Color.Green)
-                    .padding(all = 8.dp)
-                    .fillMaxWidth()
-            ) {
-                Text("Texto 5")
-                Text("Texto 6")
-            }
-            Column(
-                Modifier
-                    .padding(8.dp)
-                    .background(color = Color.DarkGray)
-                    .padding(all = 8.dp)
-            ) {
-                Text("Texto 7")
-                Text("Texto 8")
-            }
-        }
+                .size(100.dp)
+                .offset(y = (-50).dp)
+                .clip(shape = CircleShape)
+                .align(CenterHorizontally)
+        )
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
     }
 }
-
-// - BOX, ROW e COLUMN
-//@Preview(showBackground = true)
-//@Composable
-//fun ColumnPreview(){
-//    Column {
-//        Text("Texto 1")
-//        Text("Texto 2")
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun RowPreview(){
-//    Row {
-//        Text("Texto 1")
-//        Text("Texto 2")
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun BoxPreview(){
-//    Box {
-//        Text("Texto 1")
-//        Text("Texto 2")
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun CustomLayoutPreview(){
-//    Column {
-//        Text("Texto 1")
-//        Text("Texto 2")
-//        Row {
-//            Text("Texto 3")
-//            Text("Texto 4")
-//        }
-//        Box {
-//            Row {
-//                Text("Texto 5")
-//                Text("Texto 6")
-//            }
-//            Column {
-//                Text("Texto 7")
-//                Text("Texto 8")
-//            }
-//        }
-//    }
-//}
-
-// - Testes com ui no preview -
-//@Preview(
-//    name = "TextPreviewLight",
-//    showSystemUi = true
-//)
-//@Preview(
-//    name = "TextPreviewDark",
-//    showSystemUi = true,
-//    uiMode = UI_MODE_NIGHT_YES
-//)
-//@Preview(
-//    name = "TextPreview",
-//    heightDp = 200,
-//    widthDp = 300,
-//    showBackground = true,
-//    backgroundColor = 0xFFFF1144
-//)
-//@Composable
-//fun MyFirstComposablePreview(){
-//    AluveryTheme{
-//        Surface {
-//            MyFirstComposable()
-//        }
-//    }
-//}
 
---
+++ app/src/main/java/com/example/aluvery/ui/components/TestComponents.kt
@@ -0,0 +1,160 @@
+package com.example.aluvery.ui.components
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.aluvery.ui.theme.AluveryTheme
+
+@Composable
+fun MyFirstComposable() {
+    Text(text = "Meu primeiro texto")
+    Text(text = "Meu segundo texto maior")
+}
+
+//Modifiers - primeiro parâmetro opcional
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun CustomLayoutPreview() {
+    Column(
+        Modifier
+            .padding(8.dp)
+            .background(color = Color.Cyan)
+            .padding(all = 8.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Text("Texto 1")
+        Text("Texto 2")
+        Row(
+            modifier = Modifier
+                .padding(
+                    horizontal = 8.dp,
+                    vertical = 16.dp
+                )
+                .background(Color.LightGray)
+                .height(100.dp)
+                .fillMaxWidth(0.5f)
+        ) {
+            Text("Texto 3")
+            Text("Texto 4")
+        }
+        Box(
+            Modifier
+                .padding(8.dp)
+                .background(color = Color.Magenta)
+                .padding(all = 8.dp)
+                .width(200.dp)
+        ) {
+            Row(
+                Modifier
+                    .padding(8.dp)
+                    .background(color = Color.Green)
+                    .padding(all = 8.dp)
+                    .fillMaxWidth()
+            ) {
+                Text("Texto 5")
+                Text("Texto 6")
+            }
+            Column(
+                Modifier
+                    .padding(8.dp)
+                    .background(color = Color.DarkGray)
+                    .padding(all = 8.dp)
+            ) {
+                Text("Texto 7")
+                Text("Texto 8")
+            }
+        }
+    }
+}
+
+// - BOX, ROW e COLUMN
+@Preview(showBackground = true)
+@Composable
+fun ColumnPreview(){
+    Column {
+        Text("Texto 1")
+        Text("Texto 2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RowPreview(){
+    Row {
+        Text("Texto 1")
+        Text("Texto 2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoxPreview(){
+    Box {
+        Text("Texto 1")
+        Text("Texto 2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomLayoutPreview2(){
+    Column {
+        Text("Texto 1")
+        Text("Texto 2")
+        Row {
+            Text("Texto 3")
+            Text("Texto 4")
+        }
+        Box {
+            Row {
+                Text("Texto 5")
+                Text("Texto 6")
+            }
+            Column {
+                Text("Texto 7")
+                Text("Texto 8")
+            }
+        }
+    }
+}
+
+// - Testes com ui no preview -
+@Preview(
+    name = "TextPreviewLight",
+    showSystemUi = true
+)
+@Preview(
+    name = "TextPreviewDark",
+    showSystemUi = true,
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Preview(
+    name = "TextPreview",
+    heightDp = 200,
+    widthDp = 300,
+    showBackground = true,
+    backgroundColor = 0xFFFF1144
+)
+@Composable
+fun MyFirstComposablePreview(){
+    AluveryTheme{
+        Surface {
+            MyFirstComposable()
+        }
+    }
+}
