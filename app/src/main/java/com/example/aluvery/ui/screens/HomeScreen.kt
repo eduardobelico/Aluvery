27a508c1cdc80422323f2b7cf574e91bb2f1abcb
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-09-05 19:19:36 -0300

    Ajuste da barra de busca utilizando Slot Api.

--- app/src/main/java/com/example/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/aluvery/ui/screens/HomeScreen.kt
@@ -1,13 +1,23 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.aluvery.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -21,16 +31,36 @@ import com.example.aluvery.sampledata.sampleSections
 import com.example.aluvery.ui.components.ProductSection
 import com.example.aluvery.ui.theme.AluveryTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
     Column {
         var text by remember { mutableStateOf("")}
-        OutlinedTextField(value = text, onValueChange = { newValue ->
-            text = newValue
-        })
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newValue ->
+                text = newValue
+            },
+            Modifier
+                .padding(
+                    start = 16.dp,
+                    top = 16.dp,
+                    end = 16.dp
+                )
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(100),
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = "ícone de lupa")
+            },
+            label = {
+                Text(text = "Produto")
+            },
+            placeholder = {
+                Text(text = "O que você procura?")
+            }
+        )
         LazyColumn(
             Modifier
                 .fillMaxSize(),
