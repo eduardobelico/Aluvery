9431d1bcf54efc271e355b8d584f9a61fa00cf6a
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-09-08 14:45:58 -0300

    Ajuste dos textfields para receber as informacoes do usuario de acordo com a formatacao esperada do textfield.

--- app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,6 +30,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,7 +40,9 @@ import coil.compose.AsyncImage
 import com.example.aluvery.R
 import com.example.aluvery.model.Product
 import com.example.aluvery.ui.theme.AluveryTheme
+import java.lang.IllegalArgumentException
 import java.math.BigDecimal
+import java.text.DecimalFormat
 
 class ProductFormActivity : ComponentActivity() {
     
@@ -75,61 +81,96 @@ fun ProductFormScreen() {
         }
         
         if (url.isNotBlank()) {
-        AsyncImage(model = url, contentDescription = null,
-            Modifier
-                .fillMaxWidth()
-                .height(200.dp),
-            contentScale = ContentScale.Crop,
-            placeholder = painterResource(id = R.drawable.placeholder),
-            error = painterResource(id = R.drawable.placeholder)
-        )
+            AsyncImage(
+                model = url, contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
         }
         
-        TextField(value = url, onValueChange = {
-            url = it
-        },
+        TextField(
+            value = url, onValueChange = {
+                url = it
+            },
             Modifier
                 .fillMaxWidth(),
             label = {
                 Text(text = "Url da imagem")
-            })
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Next
+            )
+        )
         
         var name by remember {
             mutableStateOf("")
         }
-        TextField(value = name, onValueChange = {
-            name = it
-        },
+        TextField(
+            value = name, onValueChange = {
+                name = it
+            },
             Modifier
                 .fillMaxWidth(),
             label = {
                 Text(text = "Nome")
-            })
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+                capitalization = KeyboardCapitalization.Words
+            )
+        )
         
         var price by remember {
             mutableStateOf("")
         }
-        TextField(value = price, onValueChange = {
-            price = it
-        },
+        val formatter = remember {
+            DecimalFormat("#.##")
+        }
+        TextField(
+            value = price, onValueChange = {
+                try {
+                price = formatter.format(BigDecimal(it))
+                } catch (e: IllegalArgumentException) {
+                    if (it.isBlank()) {
+                        price = it
+                    }
+                }
+            },
             Modifier
                 .fillMaxWidth(),
             label = {
                 Text(text = "Preço")
-            })
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next
+            )
+        )
         
         var description by remember {
             mutableStateOf("")
         }
-        TextField(value = description, onValueChange = {
-            description = it
-        },
+        TextField(
+            value = description, onValueChange = {
+                description = it
+            },
             Modifier
                 .fillMaxWidth()
                 .heightIn(100.dp),
             label = {
                 Text(text = "Descrição")
-            })
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Sentences
+            )
+        )
         
         Button(
             onClick = {
@@ -138,12 +179,12 @@ fun ProductFormScreen() {
                 } catch (e: NumberFormatException) {
                     BigDecimal.ZERO
                 }
-            val product = Product(
-                name = name,
-                image = url,
-                price = convertedPrice,
-                description = description
-            )
+                val product = Product(
+                    name = name,
+                    image = url,
+                    price = convertedPrice,
+                    description = description
+                )
                 Log.i("ProductFormActivity", "ProductFormScreen: $product")
             },
             Modifier
