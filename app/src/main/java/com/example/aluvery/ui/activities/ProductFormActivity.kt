bf1156a375eb9ee0a3dba2146e6b7decf4fb0bf2
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-09-08 13:59:16 -0300

    Main Activity movida para o pacote "activities". Criada activity do formulario e finalizada a parte visual.

---
+++ app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
@@ -0,0 +1,125 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.aluvery.ui.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.aluvery.ui.theme.AluveryTheme
+
+class ProductFormActivity : ComponentActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AluveryTheme {
+                Surface {
+                    ProductFormScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductFormScreen() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "Criando o produto",
+            Modifier
+                .fillMaxWidth(),
+            fontSize = 28.sp
+        )
+        var url by remember {
+            mutableStateOf("")
+        }
+        TextField(value = url, onValueChange = {
+            url = it
+        },
+            Modifier
+                .fillMaxWidth(),
+            label = {
+                Text(text = "Url da imagem")
+            })
+        
+        var name by remember {
+            mutableStateOf("")
+        }
+        TextField(value = name, onValueChange = {
+            name = it
+        },
+            Modifier
+                .fillMaxWidth(),
+            label = {
+                Text(text = "Nome")
+            })
+        
+        var price by remember {
+            mutableStateOf("")
+        }
+        TextField(value = price, onValueChange = {
+            price = it
+        },
+            Modifier
+                .fillMaxWidth(),
+            label = {
+                Text(text = "Preço")
+            })
+        
+        var description by remember {
+            mutableStateOf("")
+        }
+        TextField(value = description, onValueChange = {
+            description = it
+        },
+            Modifier
+                .fillMaxWidth()
+                .heightIn(100.dp),
+            label = {
+                Text(text = "Descrição")
+            })
+        
+        Button(
+            onClick = { /*TODO*/ },
+            Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = "Salvar")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProductFormScreenPreview() {
+    AluveryTheme {
+        Surface {
+            ProductFormScreen()
+        }
+    }
+}
