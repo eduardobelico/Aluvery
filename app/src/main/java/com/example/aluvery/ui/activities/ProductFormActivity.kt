1b709e7021c6551857f2c0af97a8a93446d335f8
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-09-12 18:14:02 -0300

    Implementacao da viewModel da ProductFormScreen.

--- app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
@@ -5,25 +5,26 @@ package com.example.aluvery.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
-import com.example.aluvery.dao.ProductDao
 import com.example.aluvery.ui.screens.ProductFormScreen
 import com.example.aluvery.ui.theme.AluveryTheme
+import com.example.aluvery.ui.viewmodels.ProductFormScreenViewModel
 
 class ProductFormActivity : ComponentActivity() {
-    
-    private val dao = ProductDao()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AluveryTheme {
                 Surface {
-                    ProductFormScreen(onSaveClick = { product ->
-                        dao.save(product)
-                        finish()
-                    })
+                    val viewModel: ProductFormScreenViewModel by viewModels()
+                    ProductFormScreen(
+                        viewModel = viewModel,
+                        onSaveClick = {
+                            finish()
+                        })
                 }
             }
         }
---
+++ app/src/main/java/com/example/aluvery/ui/viewmodels/ProductFormScreenViewModel.kt
@@ -0,0 +1,81 @@
+package com.example.aluvery.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.aluvery.dao.ProductDao
+import com.example.aluvery.model.Product
+import com.example.aluvery.ui.states.ProductFormUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.math.BigDecimal
+import java.text.DecimalFormat
+
+class ProductFormScreenViewModel : ViewModel() {
+    
+    private val _uiState: MutableStateFlow<ProductFormUiState> = MutableStateFlow(
+        ProductFormUiState()
+    )
+    val uiState get() = _uiState.asStateFlow()
+    
+    private val dao = ProductDao()
+    
+    
+    private val formatter = DecimalFormat("#.##")
+    
+    
+    init {
+        _uiState.update { currentState ->
+            currentState.copy(
+                onUrlChange = {
+                    _uiState.value = _uiState.value.copy(
+                        url = it
+                    )
+                },
+                onNameChange = {
+                    _uiState.value = _uiState.value.copy(
+                        name = it
+                    )
+                },
+                onPriceChange = {
+                    val price = try {
+                        formatter.format(BigDecimal(it))
+                    } catch (e: IllegalArgumentException) {
+                        if (it.isEmpty()) {
+                            it
+                        } else {
+                            null
+                        }
+                    }
+                    price?.let {
+                        _uiState.value = _uiState.value.copy(
+                            price = price
+                        )
+                    }
+                },
+                onDescriptionChange = {
+                    _uiState.value = _uiState.value.copy(
+                        description = it
+                    )
+                }
+            )
+        }
+    }
+    
+    fun save() {
+        _uiState.value.run {
+            val convertedPrice = try {
+                BigDecimal(price)
+            } catch (e: NumberFormatException) {
+                BigDecimal.ZERO
+            }
+            val product = Product(
+                name = name,
+                image = url,
+                price = convertedPrice,
+                description = description
+            )
+            dao.save(product)
+        }
+    }
+    
+}
